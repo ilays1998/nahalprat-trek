94c550aa1f7a473360fc8f12226c78ebc2e8bcba
ilays <ilaysoffer@gmail.com>
2025-10-31 13:05:01 +0200

    fix import statment of package table

--- backend/routes/bookings.py
+++ backend/routes/bookings.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
-from models import db, Booking, TrekDate, AppUser
+from models import db, Booking, TrekDate, AppUser, Package
 from datetime import date, datetime
 from config import Config
 from sqlalchemy import desc, asc

ba320e52cc0c348622fe5b5bd4c48ac40f7a1f57
ilays <ilaysoffer@gmail.com>
2025-11-13 20:10:10 +0200

    Revert "improving performance"
    This reverts commit ae970db6717389e01cdae54a3120db0095d3fa5c.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      // Enable Fast Refresh in development
-      fastRefresh: true
-    })
-  ],
+  plugins: [react()],
   esbuild: {
     loader: 'jsx',
     include: /src\/.*\.[jt]sx?$/,
@@ -32,25 +27,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    cssCodeSplit: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Split vendor chunks for better caching
-          vendor: ['react', 'react-dom'],
-          router: ['react-router-dom'],
-        },
-        // Optimize chunk naming for better caching
-        chunkFileNames: 'assets/[name]-[hash].js',
-        entryFileNames: 'assets/[name]-[hash].js',
-        assetFileNames: 'assets/[name]-[hash].[ext]'
-      }
-    },
-    // Enable CSS minification and tree-shaking
-    cssMinify: true,
-    minify: 'esbuild',
-    // Reduce chunk size warnings
-    chunkSizeWarningLimit: 1000
+    sourcemap: true
   }
 }) 
